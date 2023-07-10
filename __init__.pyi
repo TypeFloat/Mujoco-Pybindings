daaa75007979611e9b50d481e8386143c5c93426
typefloat <liuzekun_123@163.com>
2023-07-10 17:18:42 +0800

    init commit

--- __init__.pyi
+++ __init__.pyi
@@ -9,3 +9,27 @@ def mj_name2id(m, type, name): # real signature unknown; restored from __doc__
     Get id of object with specified name, return -1 if not found; type is mjtObj.
     """
     pass
+
+def mj_step(m, d, nstep=1): # real signature unknown; restored from __doc__
+    """
+    mj_step(m: mujoco._structs.MjModel, d: mujoco._structs.MjData, nstep: int = 1) -> None
+    
+    Advance simulation, use control callback to obtain external force and control. Optionally, repeat nstep times.
+    """
+    pass
+
+def mj_step1(m, d): # real signature unknown; restored from __doc__
+    """
+    mj_step1(m: mujoco._structs.MjModel, d: mujoco._structs.MjData) -> None
+    
+    Advance simulation in two steps: before external force and control is set by user.
+    """
+    pass
+
+def mj_step2(m, d): # real signature unknown; restored from __doc__
+    """
+    mj_step2(m: mujoco._structs.MjModel, d: mujoco._structs.MjData) -> None
+    
+    Advance simulation in two steps: after external force and control is set by user.
+    """
+    pass
